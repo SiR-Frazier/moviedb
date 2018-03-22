83b2d58fbe57130e3b89441119b79de4a715ee50
Bryce Frazier and Ron Craig <bryce@>
2018-03-21 17:29:08 -0700

    start an actor search and find the id number, moved business and logic together

--- src/main.js
+++ src/main.js
@@ -4,4 +4,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
 
+  $('#search').click(function() {
+    let person = $('#person').val();
+    let person2 = $('#person').val();
+    for(let i = 0; i < person.length; i++) {
+      person2 = person.replace(" ", "+");
+    }
+    console.log("person: " + person);
+    console.log("person with +s: " + person2);
+      $.get(`https://api.themoviedb.org/3/search/person?api_key=${process.env.API_KEY}&query=${person}`).then(function(response) {
+        console.log(response);
+      $('.showFilms').text(`Here is ` + person + ` ID number: ` + `${response.results[0].id}`);
+    }).fail(function(error) {
+      $('.showErrors').text(`There was an error processing your request: Please try again.`);
+    });
+    event.preventDefault();
+  });
+
 });
