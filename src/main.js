90c17b7c8a77a84b4f383bebc67dd4cd38d61450
Bryce Frazier and Ron Craig <bryce@>
2018-03-22 11:36:03 -0700

    separation of business logic and ui

--- src/main.js
+++ src/main.js
@@ -1,24 +1,21 @@
+import {Actor} from './moviedb.js';
 import './styles.css';
-import $ from 'jquery';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import $ from 'jquery';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 $(document).ready(function() {
 
   $('#search').click(function() {
-    let person = $('#person').val();
-    let person2 = $('#person').val();
-    for(let i = 0; i < person.length; i++) {
-      person2 = person.replace(" ", "+");
-    }
-    console.log("person: " + person);
-    console.log("person with +s: " + person2);
-      $.get(`https://api.themoviedb.org/3/search/person?api_key=${process.env.API_KEY}&query=${person}`).then(function(response) {
-        console.log(response);
-      $('.showFilms').text(`Here is ` + person + ` ID number: ` + `${response.results[0].id}`);
-    }).fail(function(error) {
-      $('.showErrors').text(`There was an error processing your request: Please try again.`);
-    });
     event.preventDefault();
+    let person = $('#person').val();
+    let personAPI = new Actor(person);
+    let promise = personAPI.searchActor();
+    $('.showFilms').empty();
+
+    // function output
+
   });
 
 });
--- src/moviedb.js
+++ src/moviedb.js
@@ -1,1 +1,18 @@
+// import $ from 'jquery';
 
+export class Actor {
+  constructor(person) {
+    this.person = person;
+  }
+
+  searchActor() {
+    let person = $('#person').val();
+    for(let i = 0; i < this.person.length; i++) {
+      let person = this.person.replace(" ", "+")}
+    $.get(`https://api.themoviedb.org/3/search/person?api_key=${process.env.API_KEY}&query=${person}`).then(function(response) {
+      $('.showFilms').text(`Here is  ${person}'s' ID number: ${response.results[0].id}`);
+    }).fail(function(error) {
+      $('.showErrors').text(`There was an error processing your request: Please try again.`);
+    });
+  }
+}
